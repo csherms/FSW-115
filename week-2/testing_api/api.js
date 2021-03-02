cb60f3e010c716ca2a00a3d1fcfb740a793a3a96
csherms <casey.sherman@bryanuniversity.edu>
2021-03-02 15:22:52 -0700

    first commit

--- week-2/testing_api/api.js
+++ week-2/testing_api/api.js
@@ -1,27 +1,33 @@
-var data = JSON.parse({
-    "name": "R2-D2",
-    "height": "96",
-    "mass": "32",
-    "hair_color": "n/a",
-    "skin_color": "white, blue",
-    "eye_color": "red",
-    "birth_year": "33BBY",
-    "gender": "n/a",
-    "homeworld": "http://swapi.dev/api/planets/8/",
+var data = JSON.parse(`{
+    "name": "Luke Skywalker",
+    "height": "172",
+    "mass": "77",
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "19BBY",
+    "gender": "male",
+    "homeworld": "http://swapi.dev/api/planets/1/",
     "films": [
         "http://swapi.dev/api/films/1/",
         "http://swapi.dev/api/films/2/",
         "http://swapi.dev/api/films/3/",
-        "http://swapi.dev/api/films/4/",
-        "http://swapi.dev/api/films/5/",
         "http://swapi.dev/api/films/6/"
     ],
-    "species": [
-        "http://swapi.dev/api/species/2/"
+    "species": [],
+    "vehicles": [
+        "http://swapi.dev/api/vehicles/14/",
+        "http://swapi.dev/api/vehicles/30/"
     ],
-    "vehicles": [],
-    "starships": [],
-    "created": "2014-12-10T15:11:50.376000Z",
-    "edited": "2014-12-20T21:17:50.311000Z",
-    "url": "http://swapi.dev/api/people/3/"
-})
+    "starships": [
+        "http://swapi.dev/api/starships/12/",
+        "http://swapi.dev/api/starships/22/"
+    ],
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-20T21:17:56.891000Z",
+    "url": "http://swapi.dev/api/people/1/"
+}`)
+
+
+
+
