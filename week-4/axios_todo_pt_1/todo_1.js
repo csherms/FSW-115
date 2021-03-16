a7ba00bd34d921755434bf9b51447c54069f1b23
csherms <casey.sherman@bryanuniversity.edu>
2021-03-16 15:24:21 -0600

    second commit

--- week-4/axios_todo_pt_1/todo_1.js
+++ week-4/axios_todo_pt_1/todo_1.js
@@ -1,13 +1,20 @@
 axios.get("https://api.vschool.io/caseysherman/todo")
-.then(response => {
-    console.log(response.data)
-    for (let i = 0; i < response.data.length; i++) {
-    const tasks = document.getElementsByClassName("list")[i]
-    tasks.textContent = response.data[i].title
-    if (response.data[i].completed === true) {
-        tasks.style.textDecorationLine = "line-through" 
-    }
+    .then(response => {
+        
+        console.log(response.data)
+
+        for (let i = 0; i < response.data.length; i++){
+            const tasks = document.getElementsByClassName("list")[i]
+            tasks.textContent = response.data[i].title
+
+            if (response.data[i].completed === true){
+            tasks.style.textDecorationLine = "line-through" 
+            }
+
     document.body.appendChild
     }
+
 })
-.catch(error => console.log(error))
+.catch(error => console.log(error));
+
+
